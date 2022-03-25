61e4537ed39f1fbbada1bec02e8e05c85919f0f8
Ninja Pika <76650191+ninjapika@users.noreply.github.com>
2022-03-25 19:49:44 +0530

    j7elte: init trees
    Signed-off-by: Ninja Pika <76650191+ninjapika@users.noreply.github.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,7 @@
+
+include device/samsung/exynos7580-common/BoardConfigCommon.mk
+
+TARGET_KERNEL_CONFIG := lineageos_j7elte_defconfig
+
+BOARD_KERNEL_SEPARATED_DT := true
+TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
